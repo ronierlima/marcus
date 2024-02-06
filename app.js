7481950cf0577d740ff828853f5749ea2ebfcfb7
Ronier Lima <ronier.lim@gmail.com>
2024-02-05 23:31:18 -0300

    update

---
+++ app.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer';
+import express, { json } from 'express';
+import cors from "cors"
+
+const app = express()
+
+async function scrapePage(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  const htmlContent = await page.content();
+  await browser.close();
+
+  return (htmlContent);
+}
+
+app.use(json())
+app.use(cors())
+
+app.get("/", async (req, res) => {
+    
+    res.send(await scrapePage(req.body.url))
+});
+
+app.listen(8080)
+
